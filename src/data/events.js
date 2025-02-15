72fa75c72a5103e041e9856f80fc78c0dcca4336
Arieffngg <arieffarhannail@gmail.com>
2025-02-15 23:06:02 +0700

    Fix Imgur

--- src/data/events.js
+++ src/data/events.js
@@ -2,7 +2,7 @@ const events = [
     {
         title: "HOK Bekasi Online Tournament",
         description: "Organize your bookmarks however you like. Our simple drag-n-drop interface gives you complete control over how you manage your favorite websites.", 
-        imageUrl: "https://imgur.com/Pg8hqsE",
+        imageUrl: "https://imgur.com/Pg8hqsE.jpeg",
         link: "https://bit.ly/hokbekasichampionship-online2025",
         date: '23 Februari 2025',
         quota: 16,
@@ -14,9 +14,11 @@ const events = [
     {
         title: "Godlike x HOK Bekasi Berbagi",
         description: "Organize your bookmarks however you like. Our simple drag-n-drop interface gives you complete control over how you manage your favorite websites.", 
-        imageUrl: "https://imgur.com/rJic86r",
+        imageUrl: "https://imgur.com/rJic86r.jpeg",
         link: "https://www.instagram.com/p/DEOzVsnTpqO/?img_index=1",
         date: '25 Februari 2025',
+        quota: 0,
+        registration: 'Free',
         venue: 'Yayasan Al-MaaUun Bina Insan',
         status: 'Selesai',
         color: 'bg-red-500'
@@ -24,7 +26,7 @@ const events = [
     {
         title: "HOK BEKASI x MBH",
         description: "Organize your bookmarks however you like. ", 
-        imageUrl: "https://imgur.com/qt1JVPN",
+        imageUrl: "https://imgur.com/qt1JVPN.jpeg",
         link: "https://www.instagram.com/p/DDirgkXTeT3/",
         date: '22 Desember 2024',
         quota: 8,
@@ -36,7 +38,7 @@ const events = [
     {
         title: "DG LAGA 2024 ( KUALIFIKASI BEKASI )",
         description: "Organize your bookmarks however you like. Our simple drag-n-drop interface gives you complete control over how you manage your favorite websites.", 
-        imageUrl: "https://imgur.com/aVCefSc",
+        imageUrl: "https://imgur.com/aVCefSc.jpeg",
         link: "https://www.instagram.com/p/DBsYm_xzPCf/",
         date: '3 November 2024',
         quota: 16,
@@ -48,9 +50,11 @@ const events = [
     {
         title: "Nobar Honor of Kings Championship 2024",
         description: "Organize your bookmarks however you like. Our simple drag-n-drop interface gives you complete control over how you manage your favorite websites.", 
-        imageUrl: "https://imgur.com/2VuRY5T",
+        imageUrl: "https://imgur.com/2VuRY5T.jpeg",
         link: "https://www.instagram.com/p/DBowtXiBsl6/?img_index=1",
         date: '27 Oktober 2024',
+        quota: 16,
+        registration: 'Free',
         venue: 'Lippo Mall Kemang',
         status: 'Selesai',
         color: 'bg-red-500'
@@ -58,7 +62,7 @@ const events = [
     {
         title: "Gathering",
         description: "Organize your bookmarks however you like. Our simple drag-n-drop interface gives you complete control over how you manage your favorite websites.", 
-        imageUrl: "https://imgur.com/PpDfh0e",
+        imageUrl: "https://imgur.com/PpDfh0e.jpeg",
         link: "https://www.instagram.com/p/C_mdAsLzd6A/",
         date: '15 September 2024',
         quota: 8,
@@ -70,7 +74,7 @@ const events = [
     {
         title: "King City Tournament 2024",
         description: "Organize your bookmarks however you like. Our simple drag-n-drop interface gives you complete control over how you manage your favorite websites.", 
-        imageUrl: "https://imgur.com/lMnvLpe",
+        imageUrl: "https://imgur.com/lMnvLpe.jpeg",
         link: "https://www.instagram.com/p/C9zVFlPTCxq/",
         date: '4 Agustus 2024',
         quota: 16,
